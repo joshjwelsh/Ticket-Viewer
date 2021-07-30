8f959685ce6e5a2b357541a8026cc576bb1193d7
Joshua Welsh <joshua.welsh1001@gmail.com>
2021-07-30 18:04:22 -0400

    Added GetAllTickets.go - return all tickets using api/v2/tickets endpoint

---
+++ GetAllTickets.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func GetAllTickets() func() (Ticket, error) {
+	const endpoint string = "tickets"
+	const method string = http.MethodGet
+
+	return func() (Ticket, error) {
+		var ticketList Ticket
+		resp, err := Login(method, endpoint)
+		if err != nil {
+			return ticketList, fmt.Errorf("Login failed: %v", err)
+		}
+		getHeader(resp)
+		bytesBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return ticketList, fmt.Errorf("ReadAll failed on resp.Body: %v ", err)
+		}
+		if err := json.Unmarshal(bytesBody, &ticketList); err != nil {
+			return ticketList, fmt.Errorf("Unable to unmarshal to struct: %v ", err)
+		}
+		defer resp.Body.Close()
+		return ticketList, nil
+
+	}
+
+}
+
+func getHeader(resp *http.Response) {
+	for key, val := range resp.Header {
+		fmt.Printf("%v - %v\n", key, val)
+	}
+
+}
