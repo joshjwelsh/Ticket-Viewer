5b42337220ab053cff0c86828effdd3eedeae812
Joshua Welsh <joshua.welsh1001@gmail.com>
2021-08-01 00:42:59 -0400

    Moving all global constants to const.go file

---
+++ const.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	MAX_PAGE_SIZE int  = 5
+	INVALID_VALUE int  = 99
+	MENU_OPT_1    int  = 1
+	MENU_OPT_2    int  = 2
+	MENU_OPT_3    int  = 3
+	verbose       bool = false
+)
