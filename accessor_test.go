ae6c3d52d4d749d07e8ccc87667d271eddf14f17
Joshua Welsh <joshua.welsh1001@gmail.com>
2021-08-02 14:54:57 -0400

    Added messages to assert test in accessort_test.go

--- accessor_test.go
+++ accessor_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestGetAllTickets(t *testing.T) {
 	got, err := GetAllTickets()()
-	assert.Nil(t, err)
-	assert.NotNil(t, got.Tickets)
+	assert.Nil(t, err, "GetAllTickets expected to return no error but returned an error.")
+	assert.NotNil(t, got.Tickets, "GetAllTickets expected to return response object but got nil.")
 }
